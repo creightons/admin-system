ff1cb8f871f59ae39f8b9472a4cd8bdeeae950cf
a <creightons26@gmail.com>
2017-05-08 22:24:24 -0400

    Added some models and added code to seed database

---
+++ server/models.py
@@ -0,0 +1,38 @@
+from database import db
+
+# UserType Constants (Type => ID)
+SUPERUSER = 1
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key = True, nullable = False)
+	username = db.Column(db.String(20), unique = True, nullable = False)
+	password = db.Column(db.String(20), nullable = False)
+	first_name = db.Column(db.String(20))
+	last_name = db.Column(db.String(20))
+	user_type_id = db.Column(
+		db.Integer,
+		db.ForeignKey('user_type.id'),
+		nullable = False,
+		default = SUPERUSER
+	)
+	organization_id = db.Column(db.Integer, db.ForeignKey('organization.id'))
+
+	def __init__(self, username, password):
+		self.username = username
+		self.password = password
+
+class UserType(db.Model):
+	__tablename__ = 'user_type'
+	id = db.Column(db.Integer, primary_key = True, nullable = False)
+	description = db.Column(db.String(20), nullable = False)
+	users = db.relationship('User',	backref = 'user_type', lazy = 'dynamic')
+
+	def __init__(self, description):
+		self.description = description
+
+class Organization(db.Model):
+	id = db.Column(db.Integer, primary_key = True, nullable = False)
+	name = db.Column(db.String(20), nullable = False)
+
+	def __init__(self, name):
+		self.name = name
---
+++ setup-db.py
@@ -0,0 +1,67 @@
+from server import db, app
+import os
+
+user_type_data = [
+	{ 'id': 1, 'description': 'Super User' },
+	{ 'id': 2, 'description': 'Customer' },
+]
+
+user_data = [
+	{ 'username': 'user1', 'password': 'pass1', 'user_type_id': 2, 'organization_id': 1 },
+	{ 'username': 'user2', 'password': 'pass2', 'user_type_id': 2, 'organization_id': 2 },
+]
+
+organization_data = [
+	{ 'id': 1, 'name': 'Test Inc' },
+	{ 'id': 2, 'name': 'Sample Co' },
+]
+
+def add_users(db, users):
+	for row in users:
+		db.session.execute(
+			'''
+				insert into user(username, password, user_type_id, organization_id)
+				values ( :username, :password, :user_type_id, :organization_id )
+			''',
+			row
+		)
+		#user = server.models.User(row['username'], row['password'], row['user_type'])
+		#db.session.add(user)
+
+def add_user_types(db, types):
+	for row in types:
+		#user_type = server.models.UserType(row['id'], row['description'])
+		#db.session.add(user_type)
+		db.session.execute(
+			'insert into user_type(id, description) values( :id, :description )',
+			row
+		)
+
+def add_organizations(db, organizations):
+	for row in organizations:
+		#organization = server.models.Organization(row['description'])
+		#db.session.add(organization)
+		db.session.execute(
+			'insert into organization(id, name) values( :id, :name )',
+			row
+		)
+
+def seed_database(db):
+	add_users(db, user_data)
+	add_user_types(db, user_type_data)
+	add_organizations(db, organization_data)
+	db.session.commit()
+
+def main():
+	if os.path.isfile('mydb.sqlite'):
+		os.remove('mydb.sqlite')
+
+	with app.test_request_context():
+		db.init_app(app)
+		# Import all models after connecting db to app instance
+		import server.models
+		db.create_all()
+		seed_database(db)
+
+if __name__ == '__main__':
+	main()
