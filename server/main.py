0ea05b7ace93f03f8bd6ed8dc53f567cd697a63e
Creighton <creightons26@gmail.com>
2017-05-11 18:31:09 -0400

    Added more styling

--- server/main.py
+++ server/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 path = os.path.dirname( os.path.realpath(__file__) )
 database_path = os.path.join(path, '../mydb.sqlite')
 
-app.config['FLASK_DEBUG'] = 1
+app.config['DEBUG'] = 1
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + database_path
 # Secret Key for session cookies
 app.secret_key = 'Session Secret Key'
